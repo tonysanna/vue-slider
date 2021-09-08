66f23cd38609a969b21ee61ba2ab5b99e4ad8456
Antonio <antonio.sanna2706@gmail.com>
2021-09-08 16:44:21 +0200

    Push iniziale

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        counterPhoto: 0,
+        photos: [
+            'img/image1.jpg',
+            'img/image2.jpg',
+            'img/image3.jpg',
+            'img/image4.jpg'
+        ],
+        timer: 0,
+    }
+})
